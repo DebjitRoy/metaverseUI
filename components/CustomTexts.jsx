a580557abf0a73c8ab3bc21519f38d302760bd18
DebjitRoy <debjitroy.dev@gmail.com>
2025-04-08 09:37:03 -0500

    initial commit

---
+++ components/CustomTexts.jsx
@@ -0,0 +1,18 @@
+"use client";
+import { motion } from "framer-motion";
+import { textContainer, textVariant2 } from "../utils/motion";
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((char, idx) => (
+      <motion.span key={idx} variants={textVariant2}>
+        {char}
+      </motion.span>
+    ))}
+  </motion.p>
+);
+
+export const TitleText = ({ title, textStyles }) => <h2>Title Text</h2>;
